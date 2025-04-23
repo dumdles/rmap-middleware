e9837b1980da3f527dfd91e5acd6a5e8e82c8c47
dumdles <dylansfchong@gmail.com>
2025-04-23 23:29:48 +1000

    Edited config.php to use envvars.

--- src/config.php
+++ src/config.php
@@ -5,10 +5,11 @@ ini_set('display_errors', 0); // Set to 0 in production to hide errors
 
 require_once __DIR__ . '/config_secret.php';
 
-$servername = "localhost";
-$dbusername = "root";
-$password = "";
-$dbname = "project_prototype_1";
+$servername = getenv('DB_HOST');
+$dbusername = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$dbname = getenv('DB_NAME');
+$dbport = getenv('DB_PORT') ?: 3306;
 
 function getDBconnection()
 {
