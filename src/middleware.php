16775a75463dee69eed2f61090618f36cccb6712
dylan <dylan@dylans-MacBook-Air.local>
2025-04-25 15:54:32 +0800

    Edited the import path for Autoload

--- src/middleware.php
+++ src/middleware.php
@@ -10,7 +10,7 @@ if (session_status() === PHP_SESSION_NONE) {
 require_once __DIR__ . '/helpers/logger.php';
 
 // Include Composer autoload
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/config.php';
 
 use \Firebase\JWT\JWT;
